5b82d29d6a6d722476848078cbe8f09f54346c79
Aky9821 <akshay.nagar0101@outlook.com>
2021-04-14 11:27:13 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_links');
+    const navlinks = document.querySelectorAll('.nav_links li')
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav_active');
+
+        navlinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+
+            } else
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/9+0.3}s`;
+        });
+
+        burger.classList.toggle('toggle');
+    });
+
+
+}
+navSlide();
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
