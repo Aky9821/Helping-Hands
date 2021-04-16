56f20bda5fc022e8e3660765eadfbf999e867298
Aky9821 <akshay.nagar0101@outlook.com>
2021-04-16 09:21:56 +0530

    11th fixed

--- app1.js
+++ app1.js
@@ -1,3 +1,4 @@
+console.log(5 + 6);
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav_links');
--- app2.js
+++ app2.js
@@ -21,12 +21,3 @@ const navSlide = () => {
 }
 navSlide();
 
-function formFunction() {
-    var x = document.getElementById("frm1");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length; i++) {
-        text += x.elements[i].value + "<br>";
-    }
-    document.getElementById("demo").innerHTML = text;
-}
